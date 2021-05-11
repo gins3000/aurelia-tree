9b41a063b33c4b1dd722d76da4d884d1ff0e8752
Matthias Heinisch <38611039+gins3000@users.noreply.github.com>
2021-05-11 20:43:45 +0200

    chore(plugin): replace old aurelia files with new aurelia files

--- doc/article/en-US/aurelia-tree-plugin.md
+++
@@ -1,8 +0,0 @@
----
-name: Aurelia Tree
-description: A simple tree component plugin for Aurelia
-author: Matthias Heinisch (http://github.com/gins3000)
----
-## Introduction
-
-Work in progress
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import {FrameworkConfiguration} from 'aurelia-framework';
+import {PLATFORM} from 'aurelia-pal';
+
+export function configure(config: FrameworkConfiguration): void {
+  config.globalResources([
+    PLATFORM.moduleName('./elements/hello-world')
+  ]);
+}
